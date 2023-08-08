6837ff171eb6b6a92d8837fd2fda9cf0260552e2
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2023-08-08 14:37:14 -0400

    Delete decrypt.sh

--- .github/scripts/decrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export GPG_TTY=$(tty)
-mkdir config
-ls -la
-gpg --quiet --batch --yes --decrypt --passphrase="$CONFIG_INI_PASSPHRASE" \
---output ./config/config.ini config.ini.gpg
-ls -la ./config
