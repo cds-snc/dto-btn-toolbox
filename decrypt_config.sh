238cb0065ee5650a24c2ee96b0d317f7acb4341e
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2023-11-21 14:07:20 -0500

    Update decrypt_config.sh
    Now accounts for the cacert.pem and the config.ini

--- decrypt_config.sh
+++ decrypt_config.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+
+# Set GPG_TTY to the current tty
 export GPG_TTY=$(tty)
-mkdir config
-ls -la
-gpg --quiet --batch --yes --decrypt --passphrase="$CONFIG_INI_PASSPHRASE" \
---output ./config/config.ini config.ini.gpg
+
+
+# Decrypt config.ini
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+    --output ./config/config.ini config/config.ini.gpg
+
+# Decrypt cacert-2023-01-10.pem
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+    --output ./config/cacert-2023-01-10.pem config/cacert-2023-01-10.pem.gpg
+
+# List contents of the config directory
 ls -la ./config
