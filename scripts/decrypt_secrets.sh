9393fe8f6ff5b81acd9fff56c149bf9749621208
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2023-11-22 09:24:12 -0500

    move decrypt script into scripts folder

--- scripts/decrypt_secrets.sh
+++ scripts/decrypt_secrets.sh
@@ -3,14 +3,13 @@
 # Set GPG_TTY to the current tty
 export GPG_TTY=$(tty)
 
-
-# Decrypt config.ini
+# Decrypt config.ini.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-    --output ./config/config.ini config/config.ini.gpg
+    --output ./config/config.ini ./config/config.ini.gpg
 
-# Decrypt cacert-2023-01-10.pem
+# Decrypt cacert-2023-01-10.pem.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-    --output ./config/cacert-2023-01-10.pem config/cacert-2023-01-10.pem.gpg
+    --output ./config/cacert-2023-01-10.pem ./config/cacert-2023-01-10.pem.gpg
 
 # List contents of the config directory
 ls -la ./config
