9041db746b7cbee3b287b5ae7b5aca4c0e213c6c
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2024-01-08 13:38:10 -0500

    use mongo_db_write connection string

--- src/mongodb_archive/mongoDB_archive_script.py
+++ src/mongodb_archive/mongoDB_archive_script.py
@@ -5,21 +5,22 @@ from datetime import datetime, timedelta
 
 # get api key from config file and get data from AirTabe
 config = ConfigParser()
-config.read('config/config.ini')
-dbConnectionString = config.get('default', 'mongo_db')
+
+config.read("../../config/config.ini")
+dbConnectionString = config.get("default", "mongo_db_write")
 
 client = MongoClient(dbConnectionString)
 print("Connected to DB.")
 problem = client.pagesuccess.problem
 
-print('Fetched the problem collection.')
+print("Fetched the problem collection.")
 
 problemCount = problem.count_documents({})
 print("Size before removal:")
 print(problemCount)
 
 # Archive documents older than...
-N_DAYS_AGO = 90
+N_DAYS_AGO = 100
 
 today = datetime.now()
 n_days_ago = today - timedelta(days=N_DAYS_AGO)
