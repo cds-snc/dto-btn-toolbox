5d1f5d8c6acef3400a17f1707a32eb5a3f2b1741
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2024-04-29 14:14:40 +0000

    Remove decryption of config.ini as secrets are managed via GitHub

--- scripts/decrypt_secrets.sh
+++ scripts/decrypt_secrets.sh
@@ -3,10 +3,6 @@
 # Set GPG_TTY to the current tty
 export GPG_TTY=$(tty)
 
-# Decrypt config.ini.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$APPLICATION_SECRETS_PASSPHRASE" \
-    --output ./config/config.ini ./config/config.ini.gpg
-
 # Decrypt cacert-2023-01-10.pem.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLICATION_SECRETS_PASSPHRASE" \
     --output ./config/cacert-2023-01-10.pem ./config/cacert-2023-01-10.pem.gpg
